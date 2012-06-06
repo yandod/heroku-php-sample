ddba1f9dadda90981843f0f6aed01b7759043376
Yusuke Ando <yandod@gmail.com>
2012-06-06 13:17:56 +0900

    update

--- index2.php
+++ index2.php
@@ -2,7 +2,7 @@
 require_once 'sdk/src/facebook.php';
 $facebook = new Facebook(array(
   'appId'  => getenv('FACEBOOK_APP_ID'),
-  'secret' => getenv('FACEBOOK_SECRET',
+  'secret' => getenv('FACEBOOK_SECRET'),
 ));
 
 $zack = $facebook->api('/zack');
